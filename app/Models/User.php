a225a582423d18891f11716f4a21408378db82bd
Omelette719 <azwinhakim35@gmail.com>
2026-06-23 14:49:09 +0800

    Minor changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+    use HasFactory, Notifiable;
 
     protected $fillable = [
         'name', 'email', 'password', 'role', 'animal_avatar',
@@ -27,7 +26,6 @@ class User extends Authenticatable
             'balance'                  => 'decimal:2',
             'storage_used_gb'          => 'decimal:3',
             'storage_plan_expires_at'  => 'datetime',
-            'two_factor_confirmed_at'  => 'datetime',
         ];
     }
 
